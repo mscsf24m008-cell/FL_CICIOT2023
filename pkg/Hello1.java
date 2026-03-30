76ed78607146c970b4ffefb22c27363fc3abeccc
mscsf24m008-cell <mscsf24m008@pucit.edu.pk>
2026-03-30 13:31:04 +0500

    Add Hello1.java with main and printMessage methods

---
+++ pkg/Hello1.java
@@ -0,0 +1,16 @@
+
+package pkg;
+
+public class Hello1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java");
+        int x = 10; 
+        System.out.println("Hello, Checkstyle!");
+        printMessage("Testing the plugin");
+    }
+
+    public static void printMessage(String msg) {
+        System.out.println(msg);
+    }
+}
+
